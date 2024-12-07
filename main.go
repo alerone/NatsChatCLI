3fd3dc22f6bae5c10f61bcf345e9308a746cbab4
Álvaro López <lopezalvarezalvaro1@gmail.com>
2024-12-07 19:03:38 +0100

    refactor: texto informativo mejor

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 func init() {
 	err := config.GetClientArgs()
 	if err != nil {
-		log.Fatal("Usage: chat-cli <NATS_SERVER> <CHANNEL> <YOUR_NAME>")
+		log.Fatal("Usage: ./natsChat <NATS_SERVER> <CHANNEL> <YOUR_NAME>")
 	}
 	err = config.ConnectToNats()
 
